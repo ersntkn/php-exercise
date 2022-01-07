2ccc574e5362c4d273c3f207027bb7fdd7c66ff8
Ersin <ersintekinelb@gmail.com>
2022-01-07 08:14:57 +0300

    last-changes

--- panel/panel_include/aside_menu.php
+++ panel/panel_include/aside_menu.php
@@ -1,12 +1,12 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <a href="#" class="brand-link">
-    <img src="./assets/images/icons/favicon.ico" alt="Aurora Creation" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <img src="./assets/images/aurora.png" alt="Aurora Creation" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light">Aurora Creation</span>
   </a>
   <div class="sidebar">
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
       <div class="image">
-        <img src="./assets/images/icons/favicon.ico" class="img-circle elevation-2" alt="User Image">
+      
       </div>
       <div class="info">
         <a href="#" class="d-block"><?php echo $_SESSION["user"]["name"]; ?></a>
